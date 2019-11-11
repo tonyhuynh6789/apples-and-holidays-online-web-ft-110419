e1357da9e056dba58367d48d925716643d30bfed
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-11 17:17:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,6 +73,9 @@ def all_supplies_in_holidays(holiday_hash)
 
 holiday_hash.each do |season, holidays|
   puts "#{season.to_s.capitalize!}:"
+  season.each do |holidays, items|
+  string_holidays = holidays.to_s.split("_").each {|word| word.capitalize!}.join(" ")
+puts 
   
 end 
 
